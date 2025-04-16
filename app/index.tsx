ce53a4ef1371dbfbd0b428aa2e34b7c621f906c4
Weldon Fisher <chamuka15d@gmail.com>
2025-04-16 20:35:09 +0530

    First screen done

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,33 @@
-import { Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image, SafeAreaView, TouchableHighlight, TouchableOpacity } from "react-native";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
-export default function Index() {
+
+export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>ss</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Image source={require("../assets/images/Logo.png")} style={styles.logo} />
+      <StatusBar style="auto" />
+      <TouchableOpacity style={styles.button}>
+        <Text>LoginLoginLogin</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+    
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#0D0D12",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    backgroundColor: "#fff",
+    padding: 10,
+    borderRadius: 5,
+  }
+  
+
+});
