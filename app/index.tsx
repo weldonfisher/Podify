09410f912fda8538eec164f983789723c727a2a8
Weldon Fisher <chamuka15d@gmail.com>
2025-04-25 16:07:04 +0530

    Screen 01 is done

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,21 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image, SafeAreaView, TouchableHighlight, TouchableOpacity } from "react-native";
-import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { StyleSheet, Text, View, Image, SafeAreaView, TouchableOpacity } from "react-native";
 import { Link } from "expo-router";
-
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Image source={require("../assets/images/Logo.png")} style={styles.logo} />
-      View
-
-      <Link href="/welcome" style={styles.button}>
-        <Text>Get Started</Text>
+      <View style={styles.logoContainer}>
+        <Image source={require("../assets/images/Logo.png")} style={styles.logo} />
+      </View>
+      
+      <Link href="/welcome" asChild>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Get Started</Text>
+        </TouchableOpacity>
       </Link>
+      <StatusBar style="light" />
     </SafeAreaView>
-    
-    
   );
 }
 
@@ -23,19 +23,29 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#0D0D12",
+    justifyContent: "space-between",
+    paddingBottom: 50,
+  },
+  logoContainer: {
+    flex: 1,
     alignItems: "center",
     justifyContent: "center",
   },
-  
-  button: {
-    backgroundColor: "#fff",
-    padding: 10,
-    borderRadius: 12,
-
-  },
   logo: {
-
+    width: 200,
+    height: 40,
+    resizeMode: "contain",
+  },
+  button: {
+    backgroundColor: "#6C63FF",
+    padding: 16,
+    borderRadius: 8,
+    marginHorizontal: 20,
+  },
+  buttonText: {
+    color: "#FFFFFF",
+    fontSize: 16,
+    fontWeight: "600",
+    textAlign: "center",
   }
-  
-
 });
